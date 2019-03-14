e9a0fc3a3350b661706d90ddb5b81e486e050689
jmlekston <conso.lekston@gmail.com>
2019-03-14 18:17:32 +0100

    some fixes

--- the_rush_of_zombies.py
+++ the_rush_of_zombies.py
@@ -1,10 +1,10 @@
-import logging
-from threading import Thread
 import time
-import datetime
 import random
-from uuid import uuid4
+import logging
 from pytlas import on_agent_created, on_agent_destroyed, training, intent, translations
+from threading import Thread
+from datetime import datetime
+from uuid import uuid4
 
 # This entity will be shared among training data since it's not language specific
 
@@ -23,39 +23,60 @@ def en_data(): return """
 %[rush_of_zombies/quit]
   quit the game
 """
+
 class RushOfZombiesGame(Thread):
-  def __init__(self, agent_id):
+  def __init__(self, agent):
     Thread.__init__(self)
-    self.agent_id = agent_id
-    self.next_zombie_spawn_time = datetime.now().time() + random.randint(10)
-    self.exit = false
+    self.agent = agent
+    self.exit = False
+    self.zombie_spawn_delay = 10.0
+    self.zombie_spawn_date_reference = datetime.now()
+    self.plan_zombie_spawn()
   def run(self):
     while(not self.exit):
-      if datetime.now().time() > self.next_zombie_spawn_time:
-        agent[self.agent_id].answer('grrrrr')
-        self.next_spawn_time = datetime.now().time() + random.randint(10)
-      time.sleep(0.001)
+      if self.check_zombie_spawn():
+        self.agent.answer('grrrrr')
+        self.plan_zombie_spawn()
+      time.sleep(0.005)
   def stop(self):
-    self.exit = true
+    self.exit = True
+  def plan_zombie_spawn(self):
+    self.zombie_spawn_date_reference = datetime.now() 
+    self.zombie_spawn_delay =  (random.random() * 10.0 + 1)
+  def check_zombie_spawn(self):
+    if (datetime.now() - self.zombie_spawn_date_reference).total_seconds() > self.zombie_spawn_delay:
+      return True
+    else:
+      return False
 
-agents = {}
-game = None
+
+games = {}
 
 version = "v1.0"
 
-@on_agent_created()
-def when_an_agent_is_created(agt):
+stop_timeout = 1.0
+
+def stop_game(agt_id):
+  global games
+  global stop_timeout
+  game = games.pop(agt_id, None)
+  if game != None:  
+    game.stop()
+    game.join(stop_timeout)    
+    if game.is_alive():
+      raise Exception('game thread frozen')
+
+
+#@on_agent_created()
+#def when_an_agent_is_created(agt):
   # On conserve une référence à l'agent
-  agents[agt.id] = agt
+
 
 @on_agent_destroyed()
 def when_an_agent_is_destroyed(agt):
   # On devrait clear les timers pour l'agent à ce moment là
-  global game
-  if game != None:
-    game.stop()
-    game = None
-  pass
+  stop_game(agt.id)
+    
 
 @intent('help')
 def on_help(req):
@@ -64,17 +85,21 @@ def on_help(req):
 
 @intent('play')
 def on_play(req):
-  global game
-  game = RushOfZombiesGame(req.agent.id)
+  global games
+  if req.agent.id in games:
+    req.agent.answer(req._('A game is already started'))
+    return req.agent.done()
+  game = RushOfZombiesGame(req.agent)
+  games[req.agent.id] = game
   game.start()
-  req.agent.answer(req._(help_en).format(version))
+  req.agent.answer(req._('Don\'t be afraid'))
   return req.agent.done()
 
 @intent('rush_of_zombies/quit')
 def on_quit(req):
-  global game
-  if game != None:
-    game.stop()
-    game = None
+  try:
+    stop_game(agt.id)
+  except:
+    req.agent.answer(req._("Error: Unable to stop the game"))
   req.agent.answer(req._("Bye"))
   return req.agent.done()
